4fc9a74cadcdb4c3ede8655656b00dfa10153e28
Kelvin Cheung <kelvincheung247@gmail.com>
2023-11-10 02:22:57 +0800

    [Update] Updated login page

--- app/src/main/java/com/example/staffmanagementapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/staffmanagementapp/view/LoginFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.staffmanagementapp.databinding.FragmentLoginBinding
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
     private val searchViewModel: LoginViewModel by viewModels()
@@ -17,12 +19,33 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.vm = searchViewModel
+        setupView()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.vm = searchViewModel
+        lifecycleScope.launch {
+            searchViewModel.loginResponse.collect {
+                //TODO handle login response
+            }
+        }
+        lifecycleScope.launch {
+            searchViewModel.errorFlow.collect {
+                //TODO handle error message
+            }
+        }
+    }
+
+    private fun setupView() {
+        binding.btnLogin.setOnClickListener {
+            //TODO check email and password before call login API
+            searchViewModel.doLogin(
+                email = binding.edtEmail.text.toString(),
+                password = binding.edtPassword.text.toString()
+            )
+        }
     }
 
 }
--- app/src/main/java/com/example/staffmanagementapp/view/LoginViewModel.kt
+++ app/src/main/java/com/example/staffmanagementapp/view/LoginViewModel.kt
@@ -1,28 +1,62 @@
 package com.example.staffmanagementapp.view
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.staffmanagementapp.data.request.LoginRequest
 import com.example.staffmanagementapp.data.response.LoginResponse
+import com.example.staffmanagementapp.repository.StaffManagementRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class LoginViewModel : ViewModel() {
-    private val _mainLoading: MutableStateFlow<Boolean> by lazy {
-        MutableStateFlow(false)
-    }
-
-    val mainLoading: StateFlow<Boolean> by lazy {
-        _mainLoading.asStateFlow()
-    }
-
     private val _loginResponse: MutableSharedFlow<LoginResponse> by lazy {
         MutableSharedFlow()
     }
 
-    val loginResponse: SharedFlow<LoginResponse> by lazy {
+    val loginResponse by lazy {
         _loginResponse.asSharedFlow()
+    }
+
+    private val _mainLoading: MutableStateFlow<Boolean> by lazy {
+        MutableStateFlow(false)
+    }
+    val mainLoading: StateFlow<Boolean> by lazy {
+        _mainLoading.asStateFlow()
+    }
+
+    private val _errorFlow: MutableSharedFlow<String> by lazy {
+        MutableSharedFlow()
+    }
+    val errorFlow by lazy {
+        _errorFlow.asSharedFlow()
+    }
+
+    fun doLogin(email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                _mainLoading.value = true
+                val response = StaffManagementRepository.staffManagementApi.login(
+                    loginRequestBody = LoginRequest(
+                        email, password
+                    )
+                )
+                if (response.error.isNullOrEmpty() && !response.token.isNullOrEmpty()) {
+                    _loginResponse.emit(response)
+                } else {
+                    _errorFlow.emit(response.error ?: "") //TODO get unknown error from string xml
+                }
+                _mainLoading.value = false
+            } catch (e: Exception) {
+                Log.e("Login", "Do Search error: " + e.message)
+                _mainLoading.value = false
+                _errorFlow.emit(e.message ?: "") //TODO get unknown error from string xml
+            }
+        }
     }
 }
